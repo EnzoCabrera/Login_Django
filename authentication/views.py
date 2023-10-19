16ee5b9d0a60e1a2f3b0d68857ce12f223c6ace2
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-10-19 11:37:42 -0300

    Creating and configuring the Login and Logout

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from gfg import settings
+from django.core.mail import send_mail
+
+import authentication
 
 # Create your views here.
 def home(request):
@@ -17,20 +22,63 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists! Please try some other username")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered!")
+            return redirect('home')
+        
+        if len(username)>10:
+            messages.error(request, "Username must be under 10 characters")
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't match!")
+
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric")
+            return redirect('home')
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
 
         myuser.save()
 
-        messages.success(request, "Your account has been successfully created.")
+        messages.success(request, "Your account has been successfully created. We have sent you a confirmation email, please confirm tyour email in order to activate you account.")
+
+        subject = "Welcome to GFG - Django Login!!"
+        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \n Thank you for visiting our wibsite \n We have also sent you a confirmation email, pelase confirm you email address in order to activate your account. \n\n Thanking  You\n Enzo Cabrera"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
 
         return redirect('signin')
 
 
 
 def signin(request):
-    return render(request, "authnetication/signin.html")
+    
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect('home')
+
+    return render(request, "auhentication/signin.html")
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged Out Seccessfully!")
+    return redirect('home')
